9a498558994ceac625746c7b8203cdea8f174f1e
CAIQUE <CAIQUE@C-ALVES>
2021-06-23 15:06:35 -0300

    Add project files.

---
+++ Loja/Produto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loja
+{
+    class Produto
+    {
+        private string _nome; // Encapsulamento
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()//CONSTRUTOR
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade) // Contrutor tem o mesmo Nome que a Classe .
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        public string GetNome()//Autorização Para o Usuario Exibir o Nome do Produto
+        {
+            return _nome;
+        }
+        public void SetNome(string nome) //Autorização Para o Usuario Mudar o Nome do Produto
+        {
+            if (nome != null && nome.Length > 1) // só faz a Atribuição se o nome Recebido não for Nulo e Tiver mais de um Caracteres.
+            {
+                _nome = nome;
+            }
+        }
+        public double GetPreco()//Autorização Para o Usuario Exibir o Preço do Produto
+        {
+            return _preco;
+        }
+        public void SetPreco(double preco)//Autorização Para o Usuario Modificar o Preço do Produto
+        {
+            _preco = preco;
+        }
+        public int GetQuantitade()//Autorização Para o Usuario Exibir a Quantidade do Produto
+        {
+            return _quantidade;
+        }
+        public void SetQuantidade(int quantidade)//Autorização Para o Usuario Modificar a Quantidade do Produto
+        {
+            _quantidade = quantidade;
+        }
+        public double ValorTotal()
+        {
+
+            return _preco * _quantidade;
+        }
+        public void AdicionarProduto(int quantidade)// a Função vai ser Void Pous não vai retornar valor de Saida
+        {
+            _quantidade = _quantidade + quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            _quantidade = _quantidade - quantidade;
+        }
+        public override string ToString() // Serve para Fazer a Re Ultilização  dO Código, e Deixar o Programa Mais Limpo.
+        {
+            return "Produto : " + _nome + " , Preço : $" + _preco.ToString("F2", CultureInfo.InvariantCulture)
+               + ", Quantidade : " + _quantidade + ", Valor Total : $" + ValorTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Loja/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Digite Dados do Produto! , Nome , Preço e Quantidade");
+            string[] v = Console.ReadLine().Split(',');
+            string nome = v[0];
+            double preco = double.Parse(v[1], CultureInfo.InvariantCulture);
+            int quantidade = int.Parse(v[2], CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            p.SetNome(nome);
+            p.SetPreco(preco);
+            p.SetQuantidade(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Estoque : " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite Quantos Produtos Devem Ser Adicionados ao Estoque :");
+            quantidade = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Estoque Atualizado!");
+
+            Console.WriteLine();
+            Console.Write(p);
+            Console.WriteLine();
+
+            Console.Write("Digite Quantos Produtos Irão Sair do Estoque :");
+            quantidade = int.Parse(Console.ReadLine());
+            p.RemoverProduto(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Estoque Atualizado!");
+
+            Console.WriteLine();
+            Console.WriteLine(p);
+            Console.WriteLine();
+
+            Console.WriteLine("Digitou Algo Errado ? Qual deseja Corrigir :");
+            Console.Write(" Digite 1 - Nome, 2 - Para Preço , 3 - Quantidade :");
+            Console.WriteLine();
+            int concertar = int.Parse(Console.ReadLine());
+
+            switch (concertar)
+            {
+                case 1:
+                    Console.Write("Corrigir Nome :");
+                    nome = Console.ReadLine();
+                    p.SetNome(nome);
+                    break;
+                case 2:
+                    Console.Write("Corrigir Preço :");
+                    preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    p.SetPreco(preco);
+                    break;
+                case 3:
+                    Console.WriteLine("Corrigir Quantidade :");
+                    quantidade = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    p.SetQuantidade(quantidade);
+                    break;
+                default:
+                    Console.WriteLine("Opção Invalida !");
+                    break;
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Novos Dados:" + p);
+
+
+        }
+    }
+}
